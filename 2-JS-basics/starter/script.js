e422a3676c5012d740f4bc7bb7a3b0f072798570
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-18 19:35:33 +0000

    operator precedence

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -61,7 +61,7 @@ console.log(firstName + ' ' + lastName);
 * Basic Operators
 */
 
-
+/*
 var now, yearJohn, yearMark, ageJohn, ageMark
 now = 2020;
 ageJohn = 28;
@@ -88,6 +88,80 @@ console.log(typeof ageJohn);
 console.log(typeof 'Mark is older than John');
 var x;
 console.log(typeof x);
+
+*/
+
+/**************************
+* Operator precedence
+*/
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple Operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 32 - 6 = 26
+console.log(x, y);
+
+// More operators
+
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x--;
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

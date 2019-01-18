d2e21b65bbd0b7c3c14fb0896c777e0b58bb56c9
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-18 19:18:01 +0000

    basic operators

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -30,6 +30,7 @@ var delete = 23;
 * Variable mutation and type coercion
 */
 
+/*
 var firstName = 'John';
 var age = 28;
 
@@ -53,6 +54,68 @@ alert(firstName + ' is a ' + age + ' year old '
 
 var lastName = prompt('What is his last Name?');
 console.log(firstName + ' ' + lastName);
+
+*/
+
+/*************************************
+* Basic Operators
+*/
+
+
+var now, yearJohn, yearMark, ageJohn, ageMark
+now = 2020;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - 28;
+yearMark = now - 33;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+
+// Logical Operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

1663b797cbec198823c5a29251f37e6f52a00adc
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-18 19:50:42 +0000

    first coding challenge

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -95,6 +95,7 @@ console.log(typeof x);
 * Operator precedence
 */
 
+/*
 var now = 2018;
 var yearJohn = 1989;
 var fullAge = 18;
@@ -122,10 +123,34 @@ x += 10;
 console.log(x);
 x--;
 console.log(x);
+*/
 
+/***************************************************************
+* CODING CHALLENGE 1
+Mark and John are trying to compare their BMI (Body Mass Index),
+which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (
+mass in kg and height in meter).
 
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark
+has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3.
+(Something like "Is Mark's BMI higher than John's? true").
 
+*/
 
+var markMass, markHeight, markBmi, johnMass, johnHeight, johnBmi
+markMass = 80;
+markHeight = 1.8;
+markBmi = markMass / (markHeight * markHeight);
+johnMass = 60;
+johnHeight = 1.9;
+johnBmi = johnMass / (johnHeight * johnHeight);
+
+var markBmiHigher = markBmi > johnBmi;
+
+console.log('Is Mark\'s BMI higher than John\'s?' + ' ' + markBmiHigher);
 
 
 

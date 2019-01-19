9aafb62b0229c8ab2c1bf5d0712428db213e1632
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-19 23:55:41 +0000

    my solution for the final exercise of this chapter

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -618,6 +618,7 @@ if (john.calcBmi() > mark.calcBmi()) {
 * Loops and iteration
 */
 
+/*
 // this is the foor loop
 for (var i = 0; i < 10; i++) {
   console.log(i);
@@ -631,7 +632,7 @@ for (var i = 0; i < 10; i++) {
 // i = 9, 9 < 10 true, log i to console, i++
 // i = 10, 10 < 10 FALSE, exit the loop
 
-/*
+
 var john = ['John','Smith',1990,'designer','teacher',false];
 
 for (var i= 0; i < john.length; i++) {
@@ -644,7 +645,7 @@ while (i < john.length) {
   console.log(john[i]);
   i++;
 }
-*/
+
 
 // continue and break statements
 
@@ -665,32 +666,121 @@ for (var i= 0; i < john.length; i++) {
 for(var i = john.length - 1; i >= 0; i--) {
   console.log(john[i]);
 }
+*/
+
+/*****************************
+* CODING CHALLENGE 5
+*/
+
+/*
+Remember the tip calculator challenge? Let's create a more
+advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants.
+The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than
+$50, 15% when the bill is between $50 and $200, and 10% if the
+bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+
+2. Add a method to calculate the tip
+
+3. This method should include a loop to iterate over all
+the paid bills and do the tip calculations
+
+4. As an output, create 1) a new array containing all tips,
+and 2) an array containing final paid amounts (bill + tip).
+HINT: Start with two empty arrays [] as properties and then
+fill them up in the loop.
 
 
+EXTRA AFTER FINISHING: Mark's family also went on a holiday,
+going to 4 different restaurants. The bills were $77, $375, $110,
+and $45. Mark likes to tip 20% of the bill when the bill is less
+than $100, 10% when the bill is between $100 and $300, and 25%
+if the bill is more than $300 (different than John).
 
+5. Implement the same functionality as before, this time using
+Mark's tipping rules
+6. Create a function (not a method) to calculate the average
+of a given array of tips. HINT: Loop over the array, and in
+each iteration store the current sum in a variable
+(starting from 0). After you have the sum of the array, divide
+it by the number of elements in it (that's how you calculate
+the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
 
+GOOD LUCK 😀
+*/
 
+var johnFamily = {
+    bills: [124, 48, 268, 180, 42],
+    tips: [],
+    totalPaid: [],
+    calcTips: function() {
+      for (var i = 0; i < this.bills.length; i++) {
+        var percentageTip
+        if (this.bills[i] < 50) {
+          percentageTip = 0.2;
+        } else if (this.bills[i] >= 50 && this.bills[i] < 200) {
+          percentageTip = 0.15;
+        } else {
+          percentageTip = 0.1;
+        }
+        this.tips[i] = this.bills[i] * percentageTip;
+        this.totalPaid[i] = this.tips[i] + this.bills[i];
+      }
+    }
+};
 
+var markFamily = {
+    bills: [77, 375, 100, 45],
+    tips: [],
+    totalPaid: [],
+    calcTips: function() {
+      for (var i = 0; i < this.bills.length; i++) {
+        var percentageTip
+        if (this.bills[i] < 100) {
+          percentageTip = 0.2;
+        } else if (this.bills[i] >= 100 && this.bills[i] < 300) {
+          percentageTip = 0.1;
+        } else {
+          percentageTip = 0.25;
+        }
+        this.tips[i] = this.bills[i] * percentageTip;
+        this.totalPaid[i] = this.tips[i] + this.bills[i];
+      }
+    }
+};
 
+function tipsAverage(tips) {
+   return eval(tips.join('+'))/(tips.length);
+};
 
+// Execute calculations
 
+johnFamily.calcTips();
+markFamily.calcTips();
 
+// Get the average tips for each one
 
+var johnAverage = tipsAverage(johnFamily.tips);
+var markAverage = tipsAverage(markFamily.tips);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+switch(true) {
+  case johnAverage > markAverage:
+    console.log('John tips were higher!');
+    break;
+  case markAverage > johnAverage:
+    console.log('Mark tips were higher!');
+    break;
+  default:
+    console.log('They tiped the same lul!');
+    break;
+}
 
 
 

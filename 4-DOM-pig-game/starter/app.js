98d54543f4e62d1b26c1c30197feda86434458d2
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-20 21:24:39 +0000

    reseting current scores to 0 when the game starts

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -26,8 +26,10 @@ activePlayer = 1;
 //var x = document.querySelector('#score-0').textContent;
 
 document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   // do something here // annoynimous function

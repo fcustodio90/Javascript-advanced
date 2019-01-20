ebd3470b7fea05204c6134b4b0c2c2a1d549aede
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-20 21:19:42 +0000

    building the event listener for the dice game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -15,12 +15,31 @@ score wins the game
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//var x = document.querySelector('#score-0').textContent;
 
-document.querySelector('#current-0').textContent = dice;
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // do something here // annoynimous function
+
+  // 1. Random dice number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result
+  var diceDOM = document.querySelector('.dice')
+  diceDOM.style.display = 'block';
+  diceDOM.src = `dice-${dice}.png`
+
+  // 3. Update the round score IF the rolled number was NOT a 1.
+
+
+
+});

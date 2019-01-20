85e964cafd2bada99b330318e5f8eb4d55be017b
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-20 21:23:18 +0000

    starting the game with values set as zero

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -26,6 +26,8 @@ activePlayer = 1;
 //var x = document.querySelector('#score-0').textContent;
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   // do something here // annoynimous function
@@ -35,8 +37,10 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
   // 2. Display the result
   var diceDOM = document.querySelector('.dice')
+
   diceDOM.style.display = 'block';
-  diceDOM.src = `dice-${dice}.png`
+  diceDOM.src = `dice-${dice}.png`;
+
 
   // 3. Update the round score IF the rolled number was NOT a 1.
 

57070d2b632c21fc18240398185ea966b640b979
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-20 21:37:19 +0000

    testing if condition with operators to switch players when dice roll is one

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -19,15 +19,17 @@ var scores, roundScore, activePlayer
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //var x = document.querySelector('#score-0').textContent;
 
 document.querySelector('.dice').style.display = 'none';
+
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
+
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
@@ -38,14 +40,26 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   var dice = Math.floor(Math.random() * 6) + 1;
 
   // 2. Display the result
-  var diceDOM = document.querySelector('.dice')
+  var diceDOM = document.querySelector('.dice');
+  var playerDOM = document.querySelector('#current-' + activePlayer);
 
   diceDOM.style.display = 'block';
   diceDOM.src = `dice-${dice}.png`;
 
 
   // 3. Update the round score IF the rolled number was NOT a 1.
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    playerDOM.textContent = roundScore;
+    //score[0] += dice
+  } else {
+    // switch to the next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore += dice;
+    playerDOM.textContent = roundScore;
 
+  }
 
 
 });

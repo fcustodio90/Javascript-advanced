3657d15aaa8d76eba9fd882eeef6ac846d49e116
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-22 23:39:34 +0000

    attaching functions to prototype

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -9,7 +9,17 @@ var Person = function(name, yearOfBirth, job) {
   }
 };
 
+Person.prototype.calculateAge = function() {
+  console.log(2016 - this.yearOfBirth);
+};
+
+
 var john = new Person('John', 1990, 'teacher');
 
-john.calculateAge();
+var jane = new Person('Jane', 1969, 'designer');
 
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();

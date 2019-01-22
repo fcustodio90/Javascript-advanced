13e9e586be5eaf7f7b4e4597900e4f40de916dd4
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-22 23:35:40 +0000

    example of an object inside a constructor

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -4,6 +4,12 @@ var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
+  this.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth)
+  }
 };
 
 var john = new Person('John', 1990, 'teacher');
+
+john.calculateAge();
+

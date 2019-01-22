e4b0a92b0350027170724c5a0f5b30f0c566aed8
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-22 23:52:22 +0000

    quick refactor

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -4,13 +4,16 @@ var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
-  this.calculateAge = function() {
-    console.log(2016 - this.yearOfBirth)
-  }
 };
 
 Person.prototype.calculateAge = function() {
   console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+Person.prototype.isRetired = function() {
+ this.job === 'retired' ? console.log('is retired') : console.log('not retired')
 };
 
 
@@ -20,6 +23,6 @@ var jane = new Person('Jane', 1969, 'designer');
 
 var mark = new Person('Mark', 1948, 'retired');
 
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
+john.isRetired();
+
+console.log(john);

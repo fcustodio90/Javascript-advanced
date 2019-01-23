bda7ec29f29e82953b16fb0dd2e5847929ebc62e
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-23 00:44:15 +0000

    object.create way of building

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,5 +1,6 @@
 // Function construtor
 
+/*
 var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
@@ -26,3 +27,68 @@ var mark = new Person('Mark', 1948, 'retired');
 john.isRetired();
 
 console.log(john);
+*/
+
+// Object.create
+
+var personProto = {
+  calculateAge: function () {
+    console.log(2016 - this.yearOfBirth);
+  }
+}
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+  name: { value: 'Jane' },
+  yearOfBirth: { value: 1969},
+  job: { value: 'designer'}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

7ca3a775001a0e3e2f8feeb15381780fc71a01da
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-23 00:53:56 +0000

    primitives vs objects

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -31,6 +31,7 @@ console.log(john);
 
 // Object.create
 
+/*
 var personProto = {
   calculateAge: function () {
     console.log(2016 - this.yearOfBirth);
@@ -47,7 +48,45 @@ var jane = Object.create(personProto, {
   yearOfBirth: { value: 1969},
   job: { value: 'designer'}
 });
+*/
 
+// Primitives vs objects
+
+
+// oprimitives
+var a = 23;
+var b = a;
+a = 46;
+
+console.log(a, b);
+
+
+// objects
+var obj1 = {
+  name: 'John',
+  age: 26
+};
+var obj2 = obj1
+obj1.age = 30;
+
+console.log(obj1.age, obj2.age);
+
+// functions
+
+var age = 27;
+var obj = {
+  name: 'Jonas',
+  city: 'Lisbon'
+};
+
+function change(a,b) {
+  a = 30;
+  b.city = 'San francisco';
+}
+
+change(age, obj);
+console.log(age);
+console.log(obj);
 
 
 

b7704b414511ecd8fdd7713260db27a5a5b217be
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-23 22:33:42 +0000

    small closure challenge by refactoring previous exercise

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -189,6 +189,7 @@ function game() {
 game();
 */
 
+/*
 (function () {
   var score = Math.random() * 10;
   console.log(score >= 5);
@@ -200,15 +201,53 @@ game();
   var score = Math.random() * 10;
   console.log(score >= 5 - goodLuck);
 })(5);
+*/
 
+///////////////////////////////
+// Lecture: Closures
 
+function retirement(retirementAge) {
+  return function(yearOfBirth) {
+    var a = ' years left until retirement.';
+    var age = 2016 - yearOfBirth
+    console.log((retirementAge - age) + a)
+  }
+}
 
+var retirementUS = retirement(66);
+var retirementDE = retirement(65);
+var retirementIsland = retirement(67);
 
+retirementUS(1990);
+retirementDE(1990);
+retirementIsland(1990);
 
+//retirement(66)(1990);
 
+//////////////////////////////
+// Small Challenge: Re-write the interview Question
+// using closures
 
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === 'designer') {
+      var a = ', can you please explain what UX design is?'
+      console.log(name + a);
+    } else if (job === 'teacher') {
+      var a = ', What subject do you teach?'
+      console.log(name + a);
+    } else {
+      a = ', What do you do?'
+      console.log(name + a);
+    }
+  }
+}
 
+var questionDesigner = interviewQuestion('designer');
+var questionTeacher = interviewQuestion('teacher');
 
+questionDesigner('Filipe');
+questionTeacher('Filipe');
 
 
 

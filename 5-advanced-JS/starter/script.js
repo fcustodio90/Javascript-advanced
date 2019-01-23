4acb7359a12b60435811aa3404a9a7bb985341b1
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-23 01:11:27 +0000

    passing functions as arguments

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -52,7 +52,7 @@ var jane = Object.create(personProto, {
 
 // Primitives vs objects
 
-
+/*
 // oprimitives
 var a = 23;
 var b = a;
@@ -87,10 +87,44 @@ function change(a,b) {
 change(age, obj);
 console.log(age);
 console.log(obj);
+*/
+// Lecture: Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - ( 0.67 * el ));
+  } else {
+    return -1;
+  }
+}
 
 
+var ages = arrayCalc(years, calculateAge);
+console.log(ages);
 
+var fullAges = arrayCalc(ages, isFullAge);
+console.log(fullAges);
 
+var heartRates = arrayCalc(ages, maxHeartRate)
+console.log(heartRates);
 
 
 

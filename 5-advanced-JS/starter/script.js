ca9f349b129e1796f06cf4d02cfc4a1310d016ba
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-23 23:00:53 +0000

    call apply and bind

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -268,20 +268,32 @@ var john = {
                     + this.age + ' years old.' );
       } else if (style === 'friendly') {
         console.log('Hey! What\'s up? I\'m ' + this.name
-                    + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.'
+                    + ', I\'m a ' + this.job + ' and I\'m '
+                    + this.age + ' years old.' + ' Have a nice '
                     + timeOfDay);
       }
     }
 }
 
-john.presentation('friendly', 'morning');
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+}
 
+john.presentation('formal', 'morning');
 
+john.presentation.call(emily, 'friendly', 'afternoon');
 
+// john.presentation.apply(emily, ['friendly', 'afternoon']);
 
+var johnFriendly = john.presentation.bind(john, 'friendly');
 
+johnFriendly('morning');
+johnFriendly('night');
 
-
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
 
 
 

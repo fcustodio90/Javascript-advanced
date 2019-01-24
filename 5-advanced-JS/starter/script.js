764aee0d8ca14bab5df47b0f00af8bf54b0957b8
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-24 02:16:54 +0000

    question function constructor

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -324,22 +324,32 @@ console.log(fullJapan);
 /*
 --- Let's build a fun quiz game in the console! ---
 
-1. Build a function constructor called Question to describe a question. A question should include:
+1. Build a function constructor called Question to describe a question.
+A question should include:
 a) question itself
-b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+b) the answers from which the player can choose the correct one
+(choose an adequate data structure here, array, object, etc.)
 c) correct answer (I would use a number for this)
 
 2. Create a couple of questions using the constructor
 
 3. Store them all inside an array
 
-4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+4. Select one random question and log it on the console,
+together with the possible answers (each question should have a number)
+(Hint: write a method for the Question objects for this task).
 
-5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+5. Use the 'prompt' function to ask the user for the correct answer.
+The user should input the number of the correct answer such as you
+displayed it on Task 4.
 
-6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+6. Check if the answer is correct and print to the console whether
+the answer is correct ot nor (Hint: write another method for this).
 
-7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+7. Suppose this code would be a plugin for other programmers to use
+in their code. So make sure that all your code is private and doesn't interfere
+ with the other programmers code (Hint: we learned a special technique to do
+ exactly that).
 */
 /*
 --- Expert level ---
@@ -352,6 +362,13 @@ c) correct answer (I would use a number for this)
 
 11. Display the score in the console. Use yet another method for this.
 */
+
+var Question = function(questions, answers, solutions) {
+    this.questions = questions;
+    this.answers = answers;
+    this.solutions =  solution
+};
+
 
 
 

6e05fc611dce6d0dbc34aabf383304d6c2a310cc
filipe custodio <filipe.l.custodio@gmail.com>
2019-01-24 02:22:45 +0000

    questions and answers comments

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -354,9 +354,13 @@ in their code. So make sure that all your code is private and doesn't interfere
 /*
 --- Expert level ---
 
-8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+8. After you display the result, display the next random question,
+ so that the game never ends (Hint: write a function for this and
+ call it right after displaying the result)
 
-9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+9. Be careful: after Task 8, the game literally never ends.
+So include the option to quit the game if the user writes 'exit'
+instead of the answer. In this case, DON'T call the function from task 8.
 
 10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
 
@@ -369,8 +373,14 @@ var Question = function(questions, answers, solutions) {
     this.solutions =  solution
 };
 
+// 1- Question: Is javascript the coolest programming language in the world?
+// 1- Answer: Yes or No
 
+// 2- Question: What is the name of this course teacher?
+// 2- Answer: John, Michael, Jonas
 
+// 3- Question: What does best describe coding?
+// 3- Boring, Hard, Fun, Tedious
 
 
 
